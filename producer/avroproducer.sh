26d145cc3e3b395c4297642735ab5320bd2df4fd
Hakan Kaya <hakan_kaya54@hotmail.com>
2023-03-14 13:49:50 +0100

    small change

--- producer/avroproducer.sh
+++ producer/avroproducer.sh
@@ -10,7 +10,6 @@ rating=$(awk -v min=1 -v max=5 'BEGIN{srand(); print min+rand()*(max-min)}')
 genres=("Horror" "Action" "Fantasy")
 genre=${genres[$((RANDOM % ${#genres[@]}))]}
 
-echo "{\"id\": $id, \"MovieName\": \"$movieName\", \"Rating\" : $rating, \"Genre\":\"$genre\"}" | kafka-avro-console-producer --broker-list broker:29092 --topic movieData --property value.schema="$(< ../schemas/movie-schema.avsc)" 
-#| sudo docker exec --interactive schema-registry kafka-avro-console-producer --broker-list broker:29092 --topic movieData --property value.schema="$(< ../schemas/movie-schema.avsc)"
+echo "{\"id\": $id, \"MovieName\": \"$movieName\", \"Rating\" : $rating, \"Genre\":\"$genre\"}" | sudo docker exec --interactive schema-registry kafka-avro-console-producer --broker-list broker:29092 --topic movieData --property value.schema="$(< ../schemas/movie-schema.avsc)"
 sleep 1s
 done
